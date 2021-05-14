6064328b8a1caf515287fad47ee3cfd4fb919a2f
Marcelo Morais <m2marcelo@gmail.com>
2021-05-14 17:09:56 +0200

    Adding Authenthication   - Moved to multidex app because of errors   - adding normal mail or gmail options   - updated gradle files

--- starter/app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
+++ starter/app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
@@ -1,8 +1,15 @@
 package com.udacity.project4.authentication
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.firebase.ui.auth.AuthMethodPickerLayout
+import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.FirebaseAuth
 import com.udacity.project4.R
+import com.udacity.project4.locationreminders.RemindersActivity
+import com.udacity.project4.utils.Actions
 
 /**
  * This class should be the starting point of the app, It asks the users to sign in / register, and redirects the
@@ -12,7 +19,15 @@ class AuthenticationActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val firebaseAuth = FirebaseAuth.getInstance()
+
+        if (firebaseAuth.currentUser != null) {
+            navigateToRemindersActivity()
+            return
+        }
+
         setContentView(R.layout.activity_authentication)
+        findViewById<View>(R.id.button_Login).setOnClickListener { onLoginButtonClicked() }
 //         TODO: Implement the create account and sign in using FirebaseUI, use sign in using email and sign in using Google
 
 //          TODO: If the user was authenticated, send him to RemindersActivity
@@ -21,4 +36,46 @@ class AuthenticationActivity : AppCompatActivity() {
         //https://github.com/firebase/FirebaseUI-Android/blob/master/auth/README.md#custom-layout
 
     }
+
+    private fun navigateToRemindersActivity() {
+        startActivity(Intent(this, RemindersActivity::class.java))
+        finish()
+    }
+
+    private fun onLoginButtonClicked() {
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(
+                    listOf(
+                        AuthUI.IdpConfig.GoogleBuilder().build(),
+                        AuthUI.IdpConfig.EmailBuilder().build()
+                    )
+                )
+                .setAuthMethodPickerLayout(
+                    AuthMethodPickerLayout
+                        .Builder(R.layout.layout_signin_method)
+                        .setGoogleButtonId(R.id.button_google)
+                        .setEmailButtonId(R.id.button_email)
+                        .build()
+                )
+                .setTheme(R.style.AppTheme)
+                .build(), Actions.SIGN_IN
+        )
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode != Actions.SIGN_IN) {
+            return
+        }
+
+        if (resultCode == RESULT_OK) {
+            navigateToRemindersActivity()
+            return
+        }
+    }
+
+
 }
---
+++ starter/app/src/main/java/com/udacity/project4/utils/Actions.kt
@@ -0,0 +1,5 @@
+package com.udacity.project4.utils
+
+object Actions {
+    const val SIGN_IN = 1
+}
