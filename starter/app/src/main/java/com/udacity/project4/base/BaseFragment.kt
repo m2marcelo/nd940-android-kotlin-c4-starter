cd431f1c37ec4a9c254ac843b77a5e2cccd0df51
Marcelo Morais <m2marcelo@gmail.com>
2021-05-18 00:02:29 +0200

    Adding permissions to location and adjusting map fragment

--- starter/app/src/main/java/com/udacity/project4/base/BaseFragment.kt
+++ starter/app/src/main/java/com/udacity/project4/base/BaseFragment.kt
@@ -5,32 +5,33 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.snackbar.Snackbar
+import com.udacity.project4.utils.PermissionHandler
 
 /**
  * Base Fragment to observe on the common LiveData objects
  */
-abstract class BaseFragment : Fragment() {
+abstract class BaseFragment : PermissionHandler() {
     /**
      * Every fragment has to have an instance of a view model that extends from the BaseViewModel
      */
-    abstract val _viewModel: BaseViewModel
+    abstract val baseViewModel: BaseViewModel
 
     override fun onStart() {
         super.onStart()
-        _viewModel.showErrorMessage.observe(this, Observer {
+        baseViewModel.showErrorMessage.observe(this, Observer {
             Toast.makeText(activity, it, Toast.LENGTH_LONG).show()
         })
-        _viewModel.showToast.observe(this, Observer {
+        baseViewModel.showToast.observe(this, Observer {
             Toast.makeText(activity, it, Toast.LENGTH_LONG).show()
         })
-        _viewModel.showSnackBar.observe(this, Observer {
+        baseViewModel.showSnackBar.observe(this, Observer {
             Snackbar.make(this.view!!, it, Snackbar.LENGTH_LONG).show()
         })
-        _viewModel.showSnackBarInt.observe(this, Observer {
+        baseViewModel.showSnackBarInt.observe(this, Observer {
             Snackbar.make(this.view!!, getString(it), Snackbar.LENGTH_LONG).show()
         })
 
-        _viewModel.navigationCommand.observe(this, Observer { command ->
+        baseViewModel.navigationCommand.observe(this, Observer { command ->
             when (command) {
                 is NavigationCommand.To -> findNavController().navigate(command.directions)
                 is NavigationCommand.Back -> findNavController().popBackStack()
--- starter/app/src/main/java/com/udacity/project4/locationreminders/savereminder/selectreminderlocation/SelectLocationFragment.kt
+++ starter/app/src/main/java/com/udacity/project4/locationreminders/savereminder/selectreminderlocation/SelectLocationFragment.kt
@@ -1,68 +1,66 @@
 package com.udacity.project4.locationreminders.savereminder.selectreminderlocation
 
-
-import android.Manifest
-import android.app.Activity
-import android.content.Intent
-import android.content.IntentSender
-import android.content.pm.PackageManager
-import android.content.res.Resources
 import android.os.Bundle
-import android.util.Log
 import android.view.*
-import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
-import com.google.android.gms.common.api.ApiException
-import com.google.android.gms.common.api.ResolvableApiException
-import com.google.android.gms.location.*
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.*
-import com.google.android.material.snackbar.Snackbar
 import com.udacity.project4.R
 import com.udacity.project4.base.BaseFragment
 import com.udacity.project4.base.NavigationCommand
 import com.udacity.project4.databinding.FragmentSelectLocationBinding
 import com.udacity.project4.locationreminders.savereminder.SaveReminderViewModel
+import com.udacity.project4.utils.LocationUtils
+import com.udacity.project4.utils.PermissionsResultEvent
 import com.udacity.project4.utils.setDisplayHomeAsUpEnabled
+import com.udacity.project4.utils.toLatLng
 import org.koin.android.ext.android.inject
 
-class SelectLocationFragment : BaseFragment() {
+class SelectLocationFragment : BaseFragment(), OnMapReadyCallback {
+    override val baseViewModel: SaveReminderViewModel by inject()
 
-    //Use Koin to get the view model of the SaveReminder
-    override val _viewModel: SaveReminderViewModel by inject()
     private lateinit var binding: FragmentSelectLocationBinding
+    private lateinit var map: GoogleMap
+    private lateinit var selectedLocationMarker: Marker
+    private lateinit var placeOfInterest: PointOfInterest
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
-    ): View? {
-        binding =
-            DataBindingUtil.inflate(inflater, R.layout.fragment_select_location, container, false)
+    ): View {
+        binding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_select_location, container, false
+        )
 
-        binding.viewModel = _viewModel
         binding.lifecycleOwner = this
+        binding.onSaveButtonClicked = View.OnClickListener { onLocationSelected() }
 
         setHasOptionsMenu(true)
         setDisplayHomeAsUpEnabled(true)
 
-//        TODO: add the map setup implementation
-//        TODO: zoom to the user location after taking his permission
+        loadGoogleMap()
 //        TODO: add style to the map
 //        TODO: put a marker to location that the user selected
 
-
-//        TODO: call this function after the user confirms on the selected location
-        onLocationSelected()
-
         return binding.root
+    }
+
+    private fun loadGoogleMap() {
+        val mapFragment = childFragmentManager
+            .findFragmentByTag(getString(R.string.map_fragment)) as? SupportMapFragment
+            ?: return
+
+        mapFragment.getMapAsync(this)
     }
 
     private fun onLocationSelected() {
         //        TODO: When the user confirms on the selected location,
         //         send back the selected location details to the view model
         //         and navigate back to the previous fragment to save the reminder and add the geofence
+        baseViewModel.setSelectedLocation(placeOfInterest)
+        baseViewModel.navigationCommand.postValue(NavigationCommand.Back)
     }
 
 
@@ -70,22 +68,83 @@ class SelectLocationFragment : BaseFragment() {
         inflater.inflate(R.menu.map_options, menu)
     }
 
-    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
-        // TODO: Change the map type based on the user's selection.
-        R.id.normal_map -> {
-            true
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        fun setMapType(mapType: Int): Boolean {
+            map.mapType = mapType
+            return true
         }
-        R.id.hybrid_map -> {
-            true
+
+        return when (item.itemId) {
+            R.id.normal_map -> setMapType(GoogleMap.MAP_TYPE_NORMAL)
+            R.id.hybrid_map -> setMapType(GoogleMap.MAP_TYPE_HYBRID)
+            R.id.terrain_map -> setMapType(GoogleMap.MAP_TYPE_TERRAIN)
+            R.id.satellite_map -> setMapType(GoogleMap.MAP_TYPE_SATELLITE)
+            else -> false
         }
-        R.id.satellite_map -> {
-            true
-        }
-        R.id.terrain_map -> {
-            true
-        }
-        else -> super.onOptionsItemSelected(item)
     }
 
+    override fun onMapReady(map: GoogleMap) {
+        this.map = map
+        val markerOptions = MarkerOptions()
+            .position(map.cameraPosition.target)
+            .title(getString(R.string.dropped_pin))
+            .draggable(true)
 
+        selectedLocationMarker = map.addMarker(markerOptions)
+
+        baseViewModel.selectedPlaceOfInterest.value.let {
+            placeOfInterest = it ?: PointOfInterest(map.cameraPosition.target, null, null)
+
+            if (it == null) {
+                startAtCurrentLocation()
+            } else {
+                selectedLocationMarker.position = it.latLng
+                putCameraOn(it.latLng)
+            }
+        }
+
+        map.setOnMapClickListener {
+            selectedLocationMarker.position = it
+            placeOfInterest = PointOfInterest(it, null, null)
+        }
+
+        map.setOnPoiClickListener {
+            selectedLocationMarker.position = it.latLng
+            placeOfInterest = it
+        }
+    }
+
+    private fun locationPermissionHandler(event: PermissionsResultEvent, handler: () -> Unit) {
+        if (event.areAllGranted) {
+            handler()
+            return
+        }
+
+        if (event.shouldShowRequestRationale) {
+            baseViewModel.showSnackBar.postValue(getString(R.string.permission_denied_explanation))
+        }
+    }
+
+    private fun startAtCurrentLocation() {
+        if (!LocationUtils.hasLocationPermissions()) {
+            LocationUtils.requestPermissions {
+                locationPermissionHandler(it, this::startAtCurrentLocation)
+            }
+
+            return
+        }
+
+        LocationUtils.requestSingleUpdate {
+            selectedLocationMarker.position = it.toLatLng()
+            placeOfInterest = PointOfInterest(selectedLocationMarker.position, null, null)
+            putCameraOn(selectedLocationMarker.position)
+        }
+    }
+
+    private fun putCameraOn(latLng: LatLng) {
+        val cameraPosition = CameraPosition.fromLatLngZoom(latLng, 15f)
+        val cameraUpdate = CameraUpdateFactory.newCameraPosition(cameraPosition)
+
+        map.animateCamera(cameraUpdate)
+    }
 }
